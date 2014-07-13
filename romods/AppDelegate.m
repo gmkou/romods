1e9d778e268b511ea73eb0f24fda553f4b670121
Kosuke Nagano <gm.charlie@gmail.com>
2014-07-13 11:12:47 +0900

    Move IPAddress function to view controller.

--- romods/AppDelegate.m
+++ romods/AppDelegate.m
@@ -8,9 +8,6 @@
 
 #import "AppDelegate.h"
 
-#import <ifaddrs.h>
-#import <arpa/inet.h>
-
 @interface AppDelegate ()
 
 @end
@@ -34,7 +31,6 @@
     [self setupRoutes];
     // HTTPサーバーを起動する
     [self startServer];
-    [self getIPAddress];
     return YES;
 }
 
@@ -109,34 +105,7 @@
     [response respondWithString:@"Handled through selector"];
 }
 
-#pragma mark - GetIPAddress mehtods
 
-// Get IP Address
-- (NSString *)getIPAddress {
-    NSString *address = @"error";
-    struct ifaddrs *interfaces = NULL;
-    struct ifaddrs *temp_addr = NULL;
-    int success = 0;
-    // retrieve the current interfaces - returns 0 on success
-    success = getifaddrs(&interfaces);
-    if (success == 0) {
-        // Loop through linked list of interfaces
-        temp_addr = interfaces;
-        while(temp_addr != NULL) {
-            if(temp_addr->ifa_addr->sa_family == AF_INET) {
-                // Check if interface is en0 which is the wifi connection on the iPhone
-                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
-                    // Get NSString from C String
-                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
-                }
-            }
-            temp_addr = temp_addr->ifa_next;
-        }
-    }
-    // Free memory
-    freeifaddrs(interfaces);
-    NSLog(@"ipaddress:%@", address);
-    return address;
-    
-}
+
+
 @end
--- romods/ViewController.m
+++ romods/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+
 @interface ViewController ()
 
 @end
@@ -17,6 +20,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self getIPAddress];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +30,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - IPAddress mehtods
+
+// Get IP Address
+- (NSString *)getIPAddress {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    NSLog(@"ipaddress:%@", address);
+    
+    UILabel *label = [[UILabel alloc] init];
+    label.frame = CGRectMake(10, 10, 100, 50);
+    label.font = [UIFont fontWithName:@"AppleGothic" size:12];
+    label.text = address;
+    [self.view addSubview:label];
+    
+    return address;
+    
+}
 @end
