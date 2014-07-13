bc13282749449edf81e4f93cecb1c67c82fa371c
Kosuke Nagano <gm.charlie@gmail.com>
2014-07-13 10:59:43 +0900

    Modify to get IPAddress.

--- romods/AppDelegate.m
+++ romods/AppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "AppDelegate.h"
 
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+
 @interface AppDelegate ()
 
 @end
@@ -26,12 +29,12 @@
     
     // ドキュメントルートに「htdocs」を指定する
     [httpServer setDocumentRoot:[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"htdocs"]];
-    
+
     //setup Routes
     [self setupRoutes];
     // HTTPサーバーを起動する
     [self startServer];
-    
+    [self getIPAddress];
     return YES;
 }
 
@@ -88,11 +91,52 @@
         [response respondWithString:@"Hello!"];
     }];
     
+    [httpServer get:@"/hello/:name" withBlock:^(RouteRequest *request, RouteResponse *response) {
+        [response respondWithString:[NSString stringWithFormat:@"Hello %@!", [request param:@"name"]]];
+        NSLog(@"hello %@", [request param:@"name"]);
+    }];
+
+    [httpServer get:@"\?dialog" withBlock:^(RouteRequest *request, RouteResponse *response) {
+        // do something
+        NSLog (@"?dialog");
+    }];
+    
     // Routes can also be handled through selectors
     [httpServer handleMethod:@"GET" withPath:@"/selector" target:self selector:@selector(handleSelectorRequest:withResponse:)];
 }
 
 - (void)handleSelectorRequest:(RouteRequest *)request withResponse:(RouteResponse *)response {
     [response respondWithString:@"Handled through selector"];
+}
+
+#pragma mark - GetIPAddress mehtods
+
+// Get IP Address
+- (NSString *)getIPAddress {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL) {
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    NSLog(@"ipaddress:%@", address);
+    return address;
+    
 }
 @end
